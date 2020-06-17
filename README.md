b2f0364dced7a26044600914567914c4a72e622b
Joshua C. Magoliman <magolimanjoshua22@gmail.com>
2020-06-17 14:00:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# CAROUSEL LOADING IMAGES IN REACT
+
+- Purpose: My Project
+- Front End: HTML5, CSS3, React Responsive Carousel
+- Used JavaScript Library: React
+- Responsive Web Design: Yes
+- Type of Website: Static
+
+<h2> User Interface Screenshot </h2> 
+  <img src="SCREENSHOT/PIC1.png">
---
+++ build/precache-manifest.4fe8e631d61761db0d85c741dff94f86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a009ffa8ea88467304df",
+    "url": "/static/css/main.aad127ac.chunk.css"
+  },
+  {
+    "revision": "a009ffa8ea88467304df",
+    "url": "/static/js/main.a009ffa8.chunk.js"
+  },
+  {
+    "revision": "4595cbd3671a81f05c69",
+    "url": "/static/css/1.19900ea1.chunk.css"
+  },
+  {
+    "revision": "4595cbd3671a81f05c69",
+    "url": "/static/js/1.4595cbd3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dbefa89600d37b68be7f41a0bd81c87e",
+    "url": "/index.html"
+  }
+];
